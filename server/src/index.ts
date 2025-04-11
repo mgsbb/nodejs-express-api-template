f3dc7666574a841608688b909a229582003dad6c
Admin <108695191+mgsbb@users.noreply.github.com>
2025-04-11 23:14:43 +0530

    feat(server): use env variables
    --env-file flag in command

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
+const PORT = process.env.PORT;
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hi');
 });
 
-app.listen(3000, () => console.log(`Server at http://localhost:3000`));
+app.listen(PORT, () => console.log(`Server at http://localhost:${PORT}`));
