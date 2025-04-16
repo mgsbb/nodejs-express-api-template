e0bff0fe9bb0637e76ec8297fd27b7a2bdbc0ecc
Admin <108695191+mgsbb@users.noreply.github.com>
2025-04-16 10:41:05 +0530

    feat: get prisma client depending upon env
    For testing, use a different DATABASE_URL

--- server/src/utils/prismaClient.ts
+++ server/src/utils/prismaClient.ts
@@ -1,5 +1,15 @@
 import { PrismaClient } from '../generated/prisma';
 
-const prismaClient = new PrismaClient();
+let prismaClient: PrismaClient;
+
+if (process.env.NODE_ENV === 'development') {
+    prismaClient = new PrismaClient();
+} else if (process.env.NODE_ENV === 'test') {
+    prismaClient = new PrismaClient({
+        datasources: { db: { url: process.env.DATABASE_URL_TEST } },
+    });
+} else {
+    prismaClient = new PrismaClient();
+}
 
 export default prismaClient;
