ef46afc5ea94bc0949d273ff69afa29a8a9513ac
Admin <108695191+mgsbb@users.noreply.github.com>
2025-04-23 06:42:35 +0530

    refactor: error handler middleware
    Check if error is instance of AppError.
    Log if required, and send a sanitized response.

--- server/src/middlewares/error.middleware.ts
+++ server/src/middlewares/error.middleware.ts
@@ -1,16 +1,31 @@
 import { type ErrorRequestHandler } from 'express';
 import winstonLogger from '#src/utils/loggers/winston.logger';
+import { AppError } from '#src/utils/custom-errors.util';
 
 const errorHandler: ErrorRequestHandler = async (error, req, res, next) => {
+    // AppError is manually thrown, original error if any is logged at the source of error, otherwise it is lost.
+    // Since it is manually thrown, error.message is assured to be sanitized.
+    if (error instanceof AppError) {
+        res.status(error.statusCode).json({ message: error.message });
+        return;
+    }
+
+    // Errors that are not manually thrown are logged.
+
+    // pass the entire error object for: errors({ stack: true }) to work in winstonLogger
+    // (OR) manually handle stack
     winstonLogger.error(error.message, {
-        name: error.name,
-        // stack: error.stack,
+        label: 'error-handler',
+        // ...error does not include name and stack. name seems to be included when it is something other than "Error"
+        error: {
+            ...error,
+            name: error.name,
+            // stack: error.stack
+        },
     });
 
-    const statusCode = error.statusCode || 500;
-    const message = error.message || 'Server error';
-
-    res.status(statusCode).json({ message });
+    // Sanitized response is sent.
+    res.status(500).json({ message: 'Server error.' });
 };
 
 export default errorHandler;
