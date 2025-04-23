65028b8bf70c9fdd75118274372f6465665c61d2
Admin <108695191+mgsbb@users.noreply.github.com>
2025-04-23 06:34:36 +0530

    feat: modify custom errors to extend from AppError
    Rename custom errors with prefix HTTP.

--- server/src/utils/custom-errors.util.ts
+++ server/src/utils/custom-errors.util.ts
@@ -1,47 +1,42 @@
-export class NotFoundError extends Error {
-    statusCode = 404;
+export class AppError extends Error {
+    statusCode = 0;
+
+    constructor(
+        message: string,
+        statusCode: number,
+        name: string = 'AppError'
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.name = name;
+    }
+}
+
+export class HTTPNotFoundError extends AppError {
     constructor(message: string = 'Not Found') {
-        super(message);
-        this.name = 'NotFoundError';
+        super(message, 404, 'HTTPNotFoundError');
     }
 }
-
-export class BadRequestError extends Error {
-    statusCode = 400;
+export class HTTPBadRequestError extends AppError {
     constructor(message: string = 'Bad Request') {
-        super(message);
-        this.name = 'BadRequestError';
+        super(message, 400, 'HTTPBadRequestError');
     }
 }
 
-export class UnauthenticatedError extends Error {
-    statusCode = 401;
+export class HTTPUnauthenticatedError extends AppError {
     constructor(message: string = 'Unauthorized') {
-        super(message);
-        this.name = 'UnauthenticatedError';
+        super(message, 401, 'HTTPUnauthenticatedError');
     }
 }
 
-export class UnauthorizedError extends Error {
-    statusCode = 403;
+export class HTTPUnauthorizedError extends AppError {
     constructor(message: string = 'Forbidden') {
-        super(message);
-        this.name = 'UnauthorizedError';
+        super(message, 403, 'HTTPUnauthorizedError');
     }
 }
 
-export class ConflictError extends Error {
-    statusCode = 409;
+export class HTTPConflictError extends AppError {
     constructor(message: string = 'Conflict') {
-        super(message);
-        this.name = 'ConflictError';
+        super(message, 409, 'HTTPConflictError');
     }
 }
-
-export const customErrorNames = [
-    NotFoundError.name,
-    BadRequestError.name,
-    UnauthenticatedError.name,
-    UnauthorizedError.name,
-    ConflictError.name,
-];
