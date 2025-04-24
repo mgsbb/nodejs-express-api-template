33767fb00756ea6cc0f4dcb30eaa29716ecf562b
Admin <108695191+mgsbb@users.noreply.github.com>
2025-04-24 10:52:36 +0530

    feat: add user schema for registering user

---
+++ server/src/modules/v1/user/user.schema.ts
@@ -0,0 +1,51 @@
+import { z } from 'zod';
+
+export const VALIDATION_ERRORS_USER = {
+    NAME_START_ALPHABET: 'name must start with an alphabet',
+    NAME_MIN: 'name must contain at least 3 characters',
+    NAME_MAX: 'name must contain at most 20 characters',
+    EMAIL_REQUIRED: 'email is required',
+    EMAIL_VALID: 'email must be valid',
+    PASSWORD_REQUIRED: 'password is required',
+    PASSWORD_MIN: 'password must contain at least 8 characters',
+    PASSWORD_MAX: 'password must contain at most 128 characters',
+    PASSWORD_UPPERCASE: 'password must contain atleast one uppercase character',
+    PASSWORD_LOWERCASE: 'password must contain atleast one lowercase character',
+    PASSWORD_NUMERIC: 'password must contain atleast one numeric character',
+    PASSWORD_SPECIAL: 'password must contain atleast one special character',
+    EMAIL_PASSWORD_REQUIRED: 'name and email are required',
+    UNRECOGNIZED: 'unrecognized fields are not permitted',
+    ONE_FIELD_REQUIRED: 'atleast one field is required for updation',
+};
+
+const userSchemaBase = z.object({
+    name: z
+        .string()
+        .regex(/^[A-Za-z]/, {
+            message: VALIDATION_ERRORS_USER.NAME_START_ALPHABET,
+        })
+        .min(3, { message: VALIDATION_ERRORS_USER.NAME_MIN })
+        .max(20, { message: VALIDATION_ERRORS_USER.NAME_MAX })
+        .optional(),
+    email: z
+        .string({ required_error: VALIDATION_ERRORS_USER.EMAIL_REQUIRED })
+        .email({ message: VALIDATION_ERRORS_USER.EMAIL_VALID }),
+    password: z
+        .string({
+            required_error: VALIDATION_ERRORS_USER.PASSWORD_REQUIRED,
+        })
+        .min(8, { message: VALIDATION_ERRORS_USER.PASSWORD_MIN })
+        .max(128, { message: VALIDATION_ERRORS_USER.PASSWORD_MAX })
+        .regex(/[A-Z]/, { message: VALIDATION_ERRORS_USER.PASSWORD_UPPERCASE })
+        .regex(/[a-z]/, { message: VALIDATION_ERRORS_USER.PASSWORD_LOWERCASE })
+        .regex(/[0-9]/, { message: VALIDATION_ERRORS_USER.PASSWORD_NUMERIC })
+        .regex(/[!@#$%^&*()]/, {
+            message: VALIDATION_ERRORS_USER.PASSWORD_SPECIAL,
+        }),
+});
+
+export const userSchemaRegister = z
+    .object(userSchemaBase.shape, {
+        required_error: VALIDATION_ERRORS_USER.EMAIL_PASSWORD_REQUIRED,
+    })
+    .strict({ message: VALIDATION_ERRORS_USER.UNRECOGNIZED });
