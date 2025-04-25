119b234406678fe548c102a4c173a0428cb56fe3
Admin <108695191+mgsbb@users.noreply.github.com>
2025-04-25 16:51:51 +0530

    feat: validate user register input

--- server/src/modules/v1/user/user.router.ts
+++ server/src/modules/v1/user/user.router.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-
 import {
     handleCreateUser,
     handleLoginUser,
@@ -7,10 +6,12 @@ import {
     handleUpdateUser,
     handleDeleteUser,
 } from './user.controller';
+import { validateInput } from '#src/middlewares/validator.middleware';
+import { userSchemaRegister } from './user.schema';
 
 const userRouter = Router();
 
-userRouter.post('/users', handleCreateUser);
+userRouter.post('/users', validateInput(userSchemaRegister), handleCreateUser);
 userRouter.post('/users/login', handleLoginUser);
 userRouter.get('/users/:userId', handleGetUser);
 userRouter.patch('/users/:userId', handleUpdateUser);
