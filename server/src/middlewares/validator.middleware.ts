fafefaa17df97184a5229562d6f448b35bbe1c2d
Admin <108695191+mgsbb@users.noreply.github.com>
2025-04-26 09:31:06 +0530

    refactor!: validator parse instead of safeParse
    BREAKING CHANGE: validator no longer sends appropriate message to client.
    parse method throws error, which is caught by error middleware.
    Construction of error message should be implemented in the middleware/central error handler.

--- server/src/middlewares/validator.middleware.ts
+++ server/src/middlewares/validator.middleware.ts
@@ -1,9 +1,5 @@
 import { z, type ZodSchema } from 'zod';
 import { type Request, type Response, type NextFunction } from 'express';
-import { HTTPBadRequestError } from '#src/utils/errors/http.error';
-import winstonLogger from '#src/utils/loggers/winston.logger';
-import requestContextStorage from '#src/context/request.context';
-import { constructErrorMessage } from '#src/utils/zod.util';
 
 export function validateInput(schema: ZodSchema) {
     return (req: Request, res: Response, next: NextFunction) => {
@@ -12,24 +8,7 @@ export function validateInput(schema: ZodSchema) {
         //     throw new BadRequestError(`${req.method} body cannot be empty`);
         // }
 
-        const result = schema.safeParse(req.body);
-
-        const errors = result.error?.errors;
-
-        if (errors !== undefined) {
-            winstonLogger.log({
-                level: 'error',
-                message: 'validation error',
-                label: 'validator',
-                requestId: requestContextStorage.getContext('requestId'),
-                error: {
-                    ...result.error,
-                    stack: result.error?.stack,
-                },
-            });
-            const errorMessage = constructErrorMessage(errors);
-            throw new HTTPBadRequestError(errorMessage);
-        }
+        schema.parse(req.body);
 
         next();
     };
