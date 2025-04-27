cbaad4239e795f405557f2d12a7c58e83e480599
Admin <108695191+mgsbb@users.noreply.github.com>
2025-04-27 08:47:23 +0530

    refactor: remove logger instance from config file
    Move try/catch block with logger to index.ts.
    Add JWT_SECRET to config.

--- server/src/config.ts
+++ server/src/config.ts
@@ -5,6 +5,7 @@ const configSchema = z.object({
     NODE_ENV: z.enum(['development', 'production', 'test']),
     DATABASE_URL: z.string().nonempty(),
     DATABASE_URL_TEST: z.string().nonempty(),
+    JWT_SECRET: z.string().nonempty(),
 });
 
 const config = {
@@ -12,24 +13,11 @@ const config = {
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
     DATABASE_URL_TEST: process.env.DATABASE_URL_TEST,
+    JWT_SECRET: process.env.JWT_SECRET!,
 };
 
 export default config;
 
-/*
-NOTE:
-winston logger depends on config exported above in this file
-import at the top (before: export default config) to see the following errors:
-    npm run dev - no problem seems to occur
-    npm run test - test fails due to undefined values
-*/
-import winstonLogger from './utils/loggers/winston.logger';
-
 export function validateConfig() {
-    try {
-        configSchema.parse(config);
-    } catch (error) {
-        winstonLogger.error(error);
-        throw error;
-    }
+    configSchema.parse(config);
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,14 @@
 import app from './app';
-import winstonLogger from './utils/loggers/winston.logger';
 import config, { validateConfig } from './config';
+import winstonLogger from './utils/loggers/winston.logger';
 
-validateConfig();
+try {
+    validateConfig();
+} catch (error) {
+    // not logged to file?
+    winstonLogger.error(error);
+    process.exit(1);
+}
 
 app.listen(config.PORT, () => {
     // console.log(`Server at http://localhost:${PORT}`);
