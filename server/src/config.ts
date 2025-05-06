bb4a1a4757db73fa5de0c743eea6398480e53cfe
Admin <108695191+mgsbb@users.noreply.github.com>
2025-05-06 07:52:21 +0530

    feat: add cloudinary setup and environment variables

--- server/src/config.ts
+++ server/src/config.ts
@@ -5,13 +5,19 @@ const configSchema = z.object({
     NODE_ENV: z.enum(['development', 'production', 'test']),
     DATABASE_URL: z.string().nonempty(),
     JWT_SECRET: z.string().nonempty(),
+    CLOUDINARY_CLOUD_NAME: z.string().nonempty(),
+    CLOUDINARY_API_KEY: z.string().nonempty(),
+    CLOUDINARY_API_SECRET: z.string().nonempty(),
 });
 
 const config = {
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
-    JWT_SECRET: process.env.JWT_SECRET!,
+    JWT_SECRET: process.env.JWT_SECRET,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
 
 export default config;
