90f9409b8f9139d6f7c88807194f0af94d6bb0c0
Admin <108695191+mgsbb@users.noreply.github.com>
2025-05-07 16:24:16 +0530

    fix: jwt secret asserted to not be undefined
    Otherwise, anywhere config.JWT_SECRET used throws an error

--- server/src/config.ts
+++ server/src/config.ts
@@ -14,7 +14,8 @@ const config = {
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
-    JWT_SECRET: process.env.JWT_SECRET,
+    // jwt secret CANNOT be undefined
+    JWT_SECRET: process.env.JWT_SECRET!,
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
