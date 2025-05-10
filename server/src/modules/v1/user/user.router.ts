afd938b1bf8b313bf945483658cb1a7668d8e2ff
Admin <108695191+mgsbb@users.noreply.github.com>
2025-05-10 10:05:09 +0530

    docs: add swagger docs for POST /api/v1/users

--- server/src/modules/v1/user/user.router.ts
+++ server/src/modules/v1/user/user.router.ts
@@ -17,6 +17,65 @@ const userRouter = Router();
 
 const userController = new UserController();
 
+/**
+ *
+ * @openapi
+ * /api/v1/users:
+ *   post:
+ *     summary: Create a new user
+ *     tags:
+ *       - Users
+ *     description: Create a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: Timothy
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 example: timothy@email.com
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 example: Aa1!abcd
+ *             required:
+ *               - email
+ *               - password
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: number
+ *                   example: 12
+ *                 name:
+ *                   type: string
+ *                   example: Timory
+ *                 email:
+ *                   type: string
+ *                   example: timothy@email.com
+ *       409:
+ *         description: Conflict - email already exists
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: email already exists
+ *
+ */
 userRouter.post(
     '/users',
     validateInput(userSchemaRegister),
