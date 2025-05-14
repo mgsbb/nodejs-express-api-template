d5e4967a08e4435375e1e37428afe9a2c9292416
Admin <108695191+mgsbb@users.noreply.github.com>
2025-05-14 16:46:07 +0530

    config: add jwt secrets, expiry and cookie expiry
    Update .env.example file.
    JWT secrets and expiry for access and refresh tokens.
    Cookie expiry for access and refresh tokens.

--- server/src/config.ts
+++ server/src/config.ts
@@ -4,7 +4,32 @@ const configSchema = z.object({
     PORT: z.string().nonempty(),
     NODE_ENV: z.enum(['development', 'production', 'test']),
     DATABASE_URL: z.string().nonempty(),
-    JWT_SECRET: z.string().nonempty(),
+    JWT_SECRET_ACCESS_TOKEN: z.string().nonempty(),
+    JWT_SECRET_REFRESH_TOKEN: z.string().nonempty(),
+    JWT_EXPIRY_ACCESS_TOKEN: z
+        .string()
+        .nonempty()
+        .regex(/^\d+[smhdwy]$/, {
+            message: "Must be a valid JWT expiry string like '15m', '7d', etc.",
+        }),
+    JWT_EXPIRY_REFRESH_TOKEN: z
+        .string()
+        .nonempty()
+        .regex(/^\d+[smhdwy]$/, {
+            message: "Must be a valid JWT expiry string like '15m', '7d', etc.",
+        }),
+    COOKIE_EXPIRY_ACCESS_TOKEN: z
+        .string()
+        .nonempty()
+        .regex(/^\d+[smhdwy]$/, {
+            message: "Must be a valid JWT expiry string like '15m', '7d', etc.",
+        }),
+    COOKIE_EXPIRY_REFRESH_TOKEN: z
+        .string()
+        .nonempty()
+        .regex(/^\d+[smhdwy]$/, {
+            message: "Must be a valid JWT expiry string like '15m', '7d', etc.",
+        }),
     CLOUDINARY_CLOUD_NAME: z.string().nonempty(),
     CLOUDINARY_API_KEY: z.string().nonempty(),
     CLOUDINARY_API_SECRET: z.string().nonempty(),
@@ -15,7 +40,12 @@ const config = {
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
     // jwt secret CANNOT be undefined
-    JWT_SECRET: process.env.JWT_SECRET!,
+    JWT_SECRET_ACCESS_TOKEN: process.env.JWT_SECRET_ACCESS_TOKEN!,
+    JWT_SECRET_REFRESH_TOKEN: process.env.JWT_SECRET_REFRESH_TOKEN!,
+    JWT_EXPIRY_ACCESS_TOKEN: process.env.JWT_EXPIRY_ACCESS_TOKEN!,
+    JWT_EXPIRY_REFRESH_TOKEN: process.env.JWT_EXPIRY_REFRESH_TOKEN!,
+    COOKIE_EXPIRY_ACCESS_TOKEN: process.env.COOKIE_EXPIRY_ACCESS_TOKEN,
+    COOKIE_EXPIRY_REFRESH_TOKEN: process.env.COOKIE_EXPIRY_REFRESH_TOKEN,
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
