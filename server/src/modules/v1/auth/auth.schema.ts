60d83206f7adee9de143ce2decd03b5f5d79ecd8
Admin <108695191+mgsbb@users.noreply.github.com>
2025-05-16 00:14:37 +0530

    fix: add invalid type message for auth schema

--- server/src/modules/v1/auth/auth.schema.ts
+++ server/src/modules/v1/auth/auth.schema.ts
@@ -1,11 +1,14 @@
 import { z } from 'zod';
 
 export const VALIDATION_ERRORS_AUTH = {
+    NAME_INVALID_TYPE: 'name must be a string',
     NAME_START_ALPHABET: 'name must start with an alphabet',
     NAME_MIN: 'name must contain at least 3 characters',
     NAME_MAX: 'name must contain at most 20 characters',
+    EMAIL_INVALID_TYPE: 'email must be a string',
     EMAIL_REQUIRED: 'email is required',
     EMAIL_VALID: 'email must be valid',
+    PASSWORD_INVALID_TYPE: 'password must be a string',
     PASSWORD_REQUIRED: 'password is required',
     PASSWORD_MIN: 'password must contain at least 8 characters',
     PASSWORD_MAX: 'password must contain at most 128 characters',
@@ -21,7 +24,9 @@ export const VALIDATION_ERRORS_AUTH = {
 
 const authSchemaBase = z.object({
     name: z
-        .string()
+        .string({
+            invalid_type_error: VALIDATION_ERRORS_AUTH.NAME_INVALID_TYPE,
+        })
         .regex(/^[A-Za-z]/, {
             message: VALIDATION_ERRORS_AUTH.NAME_START_ALPHABET,
         })
@@ -29,11 +34,15 @@ const authSchemaBase = z.object({
         .max(20, { message: VALIDATION_ERRORS_AUTH.NAME_MAX })
         .optional(),
     email: z
-        .string({ required_error: VALIDATION_ERRORS_AUTH.EMAIL_REQUIRED })
+        .string({
+            required_error: VALIDATION_ERRORS_AUTH.EMAIL_REQUIRED,
+            invalid_type_error: VALIDATION_ERRORS_AUTH.EMAIL_INVALID_TYPE,
+        })
         .email({ message: VALIDATION_ERRORS_AUTH.EMAIL_VALID }),
     password: z
         .string({
             required_error: VALIDATION_ERRORS_AUTH.PASSWORD_REQUIRED,
+            invalid_type_error: VALIDATION_ERRORS_AUTH.PASSWORD_INVALID_TYPE,
         })
         .min(8, { message: VALIDATION_ERRORS_AUTH.PASSWORD_MIN })
         .max(128, { message: VALIDATION_ERRORS_AUTH.PASSWORD_MAX })
@@ -47,7 +56,7 @@ const authSchemaBase = z.object({
 
 export const authSchemaRegister = z
     .object(authSchemaBase.shape, {
-        required_error: VALIDATION_ERRORS_AUTH.EMAIL_PASSWORD_REQUIRED,
+        message: VALIDATION_ERRORS_AUTH.EMAIL_PASSWORD_REQUIRED,
     })
     .strict({ message: VALIDATION_ERRORS_AUTH.UNRECOGNIZED });
 
@@ -56,9 +65,11 @@ export const authSchemaLogin = z
         required_error: VALIDATION_ERRORS_AUTH.EMAIL_PASSWORD_REQUIRED,
     })
     .omit({ name: true })
+    // password check of min, max, characters will NOT be done
     .extend({
         password: z.string({
             required_error: VALIDATION_ERRORS_AUTH.PASSWORD_REQUIRED,
+            invalid_type_error: VALIDATION_ERRORS_AUTH.PASSWORD_INVALID_TYPE,
         }),
     })
     .strict({ message: VALIDATION_ERRORS_AUTH.UNRECOGNIZED });
